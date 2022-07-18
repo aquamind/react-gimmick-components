03c44cf11adfbc5e0381b4efc3b30f552ae598ad
Ken Ito <mind.like.aqua@gmail.com>
2022-07-18 22:55:44 +0900

    bugfix

--- src/components/Splash/index.tsx
+++ src/components/Splash/index.tsx
@@ -1,17 +1,20 @@
-import React, { FC, ReactNode } from "react";
+import React, { CSSProperties, FC, ReactNode } from "react";
 import styles from "./style.module.css";
 
 type Props = {
   delay: Number;
   duration: Number;
+  className?: string;
+  style?: CSSProperties;
   children?: ReactNode;
 };
 
-const Splash: FC<Props> = ({ delay, duration, children }) => {
+const Splash: FC<Props> = ({ delay, duration, className, style, children }) => {
   return (
     <div
-      className={styles.container}
+      className={className}
       style={{
+        ...style,
         position: "fixed",
         width: "100%",
         height: "100%",
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,4 @@
 export { LeftDrawer, RightDrawer, UpDrawer, DownDrawer } from "./Drawer";
+export { default as ScrollAnchor } from "./ScrollAnchor";
 export { default as ScrollProgress } from "./ScrollProgress";
+export { default as Splash } from "./Splash";
